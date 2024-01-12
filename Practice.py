60b1488a06d6ab2c293c2fb9f0431f298c6d5df3
Jorge Payà <jorgepayaalbero@gmail.com>
2024-01-12 14:29:38 +0100

    Practice file completed

--- Practice.py
+++ Practice.py
@@ -46,6 +46,66 @@ print('*****')
 print("find the non zero elements of the array using where method")
 print("Index of non zero elements: ", np.where(array_non_zero != 0))
 print('*****')
-
-
-
+# Exercise 08: Create a 3x3 identity matrix
+print("Exercise 08: Create a 3x3 identity matrix")
+identity_matrix = np.identity(3)
+print(identity_matrix)
+print('*****')
+# an identity matrix using eye method
+print("an identity matrix using eye method")
+identity_matrix = np.eye(3)
+print(identity_matrix)
+print('*****') 
+# Exercise 09: create variable named arr which value should be an array with 3 random values
+print("Exercise 09: create variable named arr which value should be an array with 3 random values")
+arr = np.random.random(3)
+print(arr)
+print('*****')
+# Exercise 10: Create variable named arr which value should be an array with 10 random values and find the maximum value and minimum value
+print("Exercise 10: Create variable named arr which value should be an array with 10 random values and find the maximum value and minimum value")
+arr = np.random.random(10)
+print(arr)
+print("Maximum value: ", arr.max())
+print("Minimum value: ", arr.min())
+print('*****')
+# Exercise 11: Create variable named arr which value should be an array with 10 random values and find the mean value
+print("Exercise 11: Create variable named arr which value should be an array with 10 random values and find the mean value")
+arr = np.random.random(10)
+print(arr)
+print("Mean value: ", arr.mean())
+print('*****')
+# Exercise 12: Create matrix with all values equal to 1 and add 0 as the values that are on the center of the matrix
+print("Exercise 12: Create matrix with all values equal to 1 and add 0 as the values that are on the center of the matrix")
+matrix_ones = np.ones((5, 5))
+print(matrix_ones)
+print('*****')
+matrix_ones[1:-1, 1:-1] = 0 # 1:-1 means from 1 to the last element -1 means the last element is not included 
+print(matrix_ones)
+print('*****')
+# add 0 as the border of the matrix 
+print("add 0 as the border of the matrix ")
+matrix_ones = np.ones((5, 5))
+matrix_ones[0, :] = 0 # 0 means the first element
+matrix_ones[-1, :] = 0 # -1 means the last element
+matrix_ones[:, 0] = 0 # 0 means the first element
+matrix_ones[:, -1] = 0 # -1 means the last element
+print(matrix_ones)
+print('*****')
+# Exercise 13: Create a matrix which values should be all numbers from 0 to 8 and dimension of the matrix should be 3x3
+print("Exercise 13: Create a matrix which values should be all numbers from 0 to 8 and dimension of the matrix should be 3x3")
+matrix_3x3 = np.arange(9).reshape(3, 3)
+print(matrix_3x3)
+print('*****')
+# same as above but using diag method
+print("same as above but using diag method")
+matrix_3x3 = np.diag(np.arange(9).reshape(3, 3))
+print(matrix_3x3)
+print('*****')
+# Exercise 14: Create a 8x8 matrix and fill it with a checkerboard pattern
+print("Exercise 14: Create a 8x8 matrix and fill it with a checkerboard pattern")
+matrix_8x8 = np.zeros((8, 8))
+#print(matrix_8x8)
+matrix_8x8[1::2, ::2] = 1 # 1::2 means from 1 to the last element with a step of 2 
+matrix_8x8[::2, 1::2] = 1 # ::2 means from 0 to the last element with a step of 2
+print(matrix_8x8)
+print('*****')
